10ae96a1d07b03a467c463eec5990c929e4c6cff
JuanRx19 <63820244+JuanRx19@users.noreply.github.com>
2023-11-15 02:53:57 -0500

    Finalización principal.

--- src/components/Hada.jsx
+++ src/components/Hada.jsx
@@ -1,12 +1,16 @@
 import React  from "react";
 import "../assets/stylesheets/Hada.css";
 import hadagif from '../assets/images/hada.gif';
+import dialogo from '../assets/images/dialogo.png';
 
 function Hada(){
     return(
     <>
         <div className="contenedor-hada">
             <img href="/" className="hada-gif" src={hadagif} alt="hada-gif" />
+            <div className="dialogo-contenedor">
+                <img href="/" className="dialogo" src={dialogo} alt="dialogo" />
+            </div>
         </div>
     </>
     );
