8db37004f2259034f3c7d1348443799f6a2ed82a
JuanRx19 <63820244+JuanRx19@users.noreply.github.com>
2023-11-15 02:05:30 -0500

    Navbar (No responsive) y fondo.

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import "../assets/stylesheets/Navbar.css";
+import "../assets/stylesheets/Fonts.css"
+import logo from '../assets/images/logo-navbar.png';
+function Navbar(){
+
+    return(
+    <>
+        <nav className="contenedor-navbar">
+            <img href="/" className="logo-navbar" src={logo} alt="logo" />
+            <ul className="lista-ul">
+                <li className="lista-li">
+                    <a href="#personajes" className="opcion">PERSONAJES</a>
+                </li>
+                <li className="lista-li">
+                    <a href="#producto" className="opcion">PRODUCTO</a>
+                </li>
+                <li className="lista-li">
+                    <a href="#material-didactico" className="opcion">MATERIAL DIDÁCTICO</a>
+                </li>
+                <li className="lista-li">
+                    <a href="#tips" className="opcion">TIPS</a>
+                </li>
+                <li className="lista-li">
+                    <a href="#galeria" className="opcion">GALERÍA</a>
+                </li>
+            </ul>
+        </nav>
+    </>
+    );
+}
+
+export default Navbar;
