bca824cc5ec8e87da94d993ec3b0717b98bfed3a
JuanRx19 <63820244+JuanRx19@users.noreply.github.com>
2023-11-15 13:11:48 -0500

    Actualizacion personajes - en proceso

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -12,50 +12,61 @@ import princesa from '../assets/images/personajes/princesa.png';
 
 const characters = [
     {
-      image: bufon,
-      title: 'JULIO',
-      description: 'Pilar de la alegría'
+        image: bufon,
+        title: 'JULIO',
+        description: 'Pilar de la alegría'
     },
     {
-      image: caballero,
-      title: 'MATEO',
-      description: 'Pilar del perdón'
+        image: caballero,
+        title: 'MATEO',
+        description: 'Pilar del perdón'
     },
     {
         image: hada,
         title: 'LUCÍA',
         description: 'Pilar de la Empatía'
-      },
-      {
+    },
+    {
         image: mago,
         title: 'DIEGO',
         description: 'Pilar de la responsabilidad'
-      },
-      {
+    },
+    {
         image: pirata,
         title: 'CARLOS',
         description: 'Pilar de la valentía'
-      },
-      {
+    },
+    {
         image: princesa,
         title: 'MARIA',
         description: 'Pilar de la amistad'
-      },
-    
-  ];
+    },
+
+];
 
 const Slider = () => {
-  return (
-    <Carousel className="carrusel" showThumbs={false}>
-      {characters.map((character, index) => (
-        <div key={index}>
-          <img className="personajes" src={character.image} alt={character.title} />
-          <h3 className='titulo-pj'>{character.title}</h3>
-          <p className='descripcion-pj'>{character.description}</p>
+    return (
+        <div className="contenedor-principal">
+            <div className="contenedor-titulo">
+                <h3 className='titulo-h3'>PERSONAJES</h3>
+            </div>
+            <div className="contenedor-descripcion">
+                <p className='descripcion-p'>Bienvenido a Encantopia, te presentamos a un grupo de amigos extraordinarios, cada uno con un
+                    poder único que los hace especiales. ¡Conócelos y descubre los secretos detrás de sus dones
+                    asombrosos!</p>
+            </div>
+            <Carousel className="carrusel" showThumbs={false}>
+                {characters.map((character, index) => (
+                    <div key={index}>
+                        <img className="personajes" src={character.image} alt={character.title} />
+                        <h3 className='titulo-pj'>{character.title}</h3>
+                        <p className='descripcion-pj'>{character.description}</p>
+                    </div>
+                ))}
+            </Carousel>
         </div>
-      ))}
-    </Carousel>
-  );
+
+    );
 };
 
 export default Slider;
