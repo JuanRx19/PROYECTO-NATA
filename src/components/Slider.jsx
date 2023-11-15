cd67af66f3e41d4a5e0edef31dfcdf17b1fadd21
JuanRx19 <63820244+JuanRx19@users.noreply.github.com>
2023-11-15 14:50:27 -0500

    Prueba

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Carousel } from 'react-responsive-carousel';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
 import "../assets/stylesheets/Slider.css";
-import "react-responsive-carousel/lib/styles/carousel.min.css";
 import "../assets/stylesheets/Fonts.css"
 import bufon from '../assets/images/personajes/bufon.png';
 import caballero from '../assets/images/personajes/caballero.png';
@@ -44,29 +44,44 @@ const characters = [
 
 ];
 
-const Slider = () => {
-    return (
-        <div className="contenedor-principal">
-            <div className="contenedor-titulo">
-                <h3 className='titulo-h3'>PERSONAJES</h3>
-            </div>
-            <div className="contenedor-descripcion">
-                <p className='descripcion-p'>Bienvenido a Encantopia, te presentamos a un grupo de amigos extraordinarios, cada uno con un
-                    poder único que los hace especiales. ¡Conócelos y descubre los secretos detrás de sus dones
-                    asombrosos!</p>
-            </div>
-            <Carousel className="carrusel" showThumbs={false}>
-                {characters.map((character, index) => (
-                    <div key={index}>
-                        <img className="personajes" src={character.image} alt={character.title} />
-                        <h3 className='titulo-pj'>{character.title}</h3>
-                        <p className='descripcion-pj'>{character.description}</p>
-                    </div>
-                ))}
-            </Carousel>
-        </div>
+const responsive = {
+  desktop: {
+    breakpoint: { max: 1920, min: 1080 },
+    items: 3,
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 2,
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1,
+  },
+};
 
-    );
+const Slider = () => {
+  return (
+    <div className="contenedor-principal">
+      <div className="contenedor-titulo">
+        <h3 className='titulo-h3'>PERSONAJES</h3>
+      </div>
+      <div className="contenedor-descripcion">
+        <p className='descripcion-p'>Bienvenido a Encantopia, te presentamos a un grupo de amigos extraordinarios, cada uno con un
+          poder único que los hace especiales. ¡Conócelos y descubre los secretos detrás de sus dones
+          asombrosos!</p>
+      </div>
+      <Carousel responsive={responsive} showDots={false} infinite={true}>
+        {characters.map((character, index) => (
+          <div className="carrusel" key={index}>
+            <img className="personajes" src={character.image} alt={character.title} />
+            <h3 className='titulo-pj'>{character.title}</h3>
+            <p className='descripcion-pj'>{character.description}</p>
+          </div>
+        ))}
+      </Carousel>
+      
+    </div>
+  );
 };
 
 export default Slider;
