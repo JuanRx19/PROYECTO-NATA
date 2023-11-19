24a615d3fccd422bf26e715a69a4a476286d166f
JuanRx19 <63820244+JuanRx19@users.noreply.github.com>
2023-11-19 18:27:21 -0500

    Footer terminado

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,18 +2,16 @@ import React from "react";
 import "../assets/stylesheets/Fonts.css"
 import "../assets/stylesheets/Footer.css"
 import footer from '../assets/images/footer.gif';
-import footer2 from '../assets/images/footer2.png';
 
 function Footer() {
     return (
         <>
             <div className="contenedor-footer">
                 <div className="contenedor-titulo-footer">
-                    a
+                    <h2>¡Tu eres el verdadero tesoro!</h2>
                 </div>
                 <div className="contenedor-imagenes-cuento">
                     <img className="cuento-img-1" src={footer} alt="cuento" />
-                    <img className="cuento-img-2" src={footer2} alt="cuento" />
                 </div>
 
             </div>
